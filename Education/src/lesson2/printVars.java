82cad83456b82a5630f570b61f31ad3f922d6e3e
olegme <olegme@olegme-mac.haifa.ibm.com>
2018-07-19 11:57:24 +0300

    My first commit

---
+++ Education/src/lesson2/printVars.java
@@ -0,0 +1,17 @@
+package lesson2;
+
+public class printVars {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		int a = 5;         // Declares three ints, a, b, and c.
+		byte B = 22;         // initializes a byte type variable B.
+		double pi = 3.14159; // declares and assigns a value of PI.
+		char z = 'a'; // the char variable a iis initialized with value 'a'
+		String string = "lol";
+		System.out.println("Variable int = " + a + "\n" + "Variable byte = " + B + "\n" + 
+		"Variable double = " + pi + "\n" + "Variable char = " + z + "\n" + "Variabe String = " + string);
+	}
+
+}
